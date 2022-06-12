0a2c7ae5be9d44b3a6e3bf85e60c0ce5976cf3c4
Alp Bulut <83169689+AlbusNimbus@users.noreply.github.com>
2022-06-13 01:05:32 +0200

    Implement mousedown brush

--- script.js
+++ script.js
@@ -1,20 +1,65 @@
-var size = 8;
+var size = 10;
+var color = "black";
+var ink = color;
 const canvas = document.getElementById('canvas');
+const colors = document.getElementById('color-buttons');
+const pencil = document.getElementById('draw-button');
+const eraser = document.getElementById('erase-button');
+const delet = document.getElementById('delete-button');
 
 
-function createCells(size){
+function createCells(size) {
     canvas.innerHTML = "";
-    canvas.style.gridTemplateColumns = "repeat("+ size.toString() +",1fr)"
-    canvas.style.gridTemplateRows =  "repeat("+ size.toString() +",1fr)"
-    for (var i=0; i < size*size; i++) {
+    canvas.style.gridTemplateColumns = "repeat(" + size.toString() + ",1fr)"
+    canvas.style.gridTemplateRows = "repeat(" + size.toString() + ",1fr)"
+
+    for (var i = 0; i < size * size; i++) {
         const cell = document.createElement('div')
         cell.className = 'cell'
         canvas.append(cell)
     };
+    const cells = document.querySelectorAll('.cell');
+    canvas.addEventListener('mousedown',()=>{
+        mouseDown = true;
+    });
+    canvas.addEventListener('mouseup',()=>{
+        mouseDown = false;
+    });
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseover', () => {
+            if(mouseDown){
+                cell.style.background = ink;
+            }
+        }
+        );
+    });
+    
+}
+
+function generateColors(){
+    var color = document.querySelector('#color-button')
+    for (var i = 0; i < 7; i++) {
+        var colorClone = color.cloneNode(true);
+        
+        colors.append(colorClone)
+    };
 }
 
 
+pencil.addEventListener('click', paintMode);
+eraser.addEventListener('click', eraseMode);
+delet.addEventListener('click', del);
 
+function del(){
+    createCells(size);
+}
 
+function paintMode() {
+    ink = "black"
+}
+function eraseMode(){
+    ink = "white"
+}
+generateColors();
 createCells(size);
 console.log('yes');
